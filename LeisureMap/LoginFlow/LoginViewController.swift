173b5ebb53c5f155707eb095d59881107292a71f
marissa666 <564297098@qq.com>
2018-07-25 17:54:58 +0800

    add uitextfield event

--- LeisureMap/LoginFlow/LoginViewController.swift
+++ LeisureMap/LoginFlow/LoginViewController.swift
@@ -8,15 +8,64 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController,UITextFieldDelegate {
 
+    
+    @IBOutlet weak var textAccount: UITextField!
+    
+    @IBOutlet weak var textPassword: UITextField!
+    
+    @IBOutlet weak var btnLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
+    //限制长度
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+       //只允许接受某些字源
+        let accept = "abcdeABCDE"
+        let cs = NSCharacterSet(charactersIn: accept).inverted//['a','b','c']
+        print("cs")
+        print(cs)
+        let filtered = string.components(separatedBy: cs).joined(separator: "")//["a","b","c"]
+        
+        print(filtered)
+        if string != filtered {
+            return false
+        }
+        print("======")
+        //设置最大长度
+        var maxLength:Int = 0
+       
+        if textField.tag == 1{
+            maxLength = 4//账户最长12
+        }
+        if textField.tag == 2{
+            maxLength = 5//密码长度最长8
+        }
+      
+        let currentString:NSString = textField.text! as NSString
+        let newString:NSString = currentString.replacingCharacters(in: range, with: string) as NSString
+        print(currentString)
+        print(string)
+        print(newString)
+        return newString.length <= maxLength
+     
+        
+    }
     
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.tag == 1{
+            textField.resignFirstResponder()
+            textPassword.becomeFirstResponder()
+        }
+        if textField.tag == 2{
+            textPassword.resignFirstResponder()
+        }
+        return true
+    }
+    
     /*
     // MARK: - Navigation
 
